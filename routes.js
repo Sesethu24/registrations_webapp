ec6e3b4ec622dc730450a0322c1f439979024063
Sesethu24 <seshmomoza20@gmail.com>
2019-11-12 15:16:58 +0200

    fixed some errors

--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ module.exports = function (regs) {
 
     async function index(req, res) {
         var list = await regs.getRegistrations()
-        
-        
+
+
         res.render("index", {
             regnums: list
         })
@@ -15,9 +15,12 @@ module.exports = function (regs) {
             req.flash("message", "reg can't be blank, Please enter a reg number!")
             return res.redirect('/')
         }
-        if(myRegex){
-            req.flash("message", "invalid registration number");
+        if (myRegex) {
+            req.flash("message", "INVALID! a valid reg starts with CA, CY or CJ a space and numbers");
             return res.redirect('/')
+        } 
+        else if (myRegex === false) {
+            req.flash("message", "INVALID town tag! a valid reg starts with CA, CY or CJ ");
         }
         await regs.addToList(regnumber)
         res.redirect('/');
@@ -26,10 +29,11 @@ module.exports = function (regs) {
     async function filteredRegs(req, res) {
         let myregs = req.body.town;
         let regnums = await regs.theFilter(myregs)
-        
-        
-        res.render('index', {regnums}
-        )
+
+
+        res.render('index', {
+            regnums
+        })
     }
 
     function checkForRegex(param) {
@@ -40,7 +44,7 @@ module.exports = function (regs) {
         var regex = /([A-Z]){2}\s+([0-9]){3}\s([0-9]){3}/g;
         var newReg2 = regex.test(param)
         return !newReg && !newReg2
-        
+
     }
     async function clearButton(req, res) {
         await regs.resetData()
